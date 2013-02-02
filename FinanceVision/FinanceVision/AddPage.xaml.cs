061563f98d74ecc61736a08f9ca242277db24b6e
Glen Chao <glenchao@outlook.com>
2013-02-02 14:34:54 -0500

    Added Sample Image & Changed Camera Task
    . Added sample receipt images
    . To execute this function...
    - Bring up the menu in the MainPage.xaml
    - Click devsetup from the menu
    - Press the "+" appbar button to check
    
    . Changed CameraCaptureTask to PhotoChooserTask
    - Changed to PhotoChooserTask because we can choose from the
    MediaLibrary or take picture from camera

--- FinanceVision/FinanceVision/AddPage.xaml.cs
+++ FinanceVision/FinanceVision/AddPage.xaml.cs
@@ -13,12 +13,29 @@ namespace FinanceVision
 {
     public partial class AddPage : PhoneApplicationPage
     {
-        private CameraCaptureTask cam;
+        //private CameraCaptureTask cam;
+        private PhotoChooserTask photoChooser; 
+
         public AddPage()
         {
             InitializeComponent();
-            cam = new CameraCaptureTask();
-            cam.Completed += cam_Completed;
+            //cam = new CameraCaptureTask();
+            //cam.Completed += cam_Completed;
+
+            photoChooser = new PhotoChooserTask();
+            photoChooser.ShowCamera = true;
+            photoChooser.Completed += photoChooser_Completed;
+        }
+
+        void photoChooser_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                //Code to display the photo on the page in an image control named myImage.
+                System.Windows.Media.Imaging.BitmapImage bmp = new System.Windows.Media.Imaging.BitmapImage();
+                bmp.SetSource(e.ChosenPhoto);
+                myImage.Source = bmp;
+            }
         }
 
         void cam_Completed(object sender, PhotoResult e)
@@ -36,8 +53,11 @@ namespace FinanceVision
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            if(e.NavigationMode == NavigationMode.New)
-                cam.Show();
+            if (e.NavigationMode == NavigationMode.New)
+            {
+                photoChooser.Show();
+                //cam.Show();
+            }
         }
 
     }
--- FinanceVision/FinanceVision/MainPage.xaml.cs
+++ FinanceVision/FinanceVision/MainPage.xaml.cs
@@ -8,11 +8,15 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using FinanceVision.Resources;
+using Microsoft.Xna.Framework.Media;
+using System.IO;
+using System.Windows.Media.Imaging;
 
 namespace FinanceVision
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        MediaLibrary library;
         // Constructor
         public MainPage()
         {
@@ -20,6 +24,7 @@ namespace FinanceVision
 
             // Sample code to localize the ApplicationBar
             BuildLocalizedApplicationBar();
+            library = new MediaLibrary();
         }
 
         // Code for building a localized ApplicationBar
@@ -35,8 +40,23 @@ namespace FinanceVision
             ApplicationBar.Buttons.Add(appBarButton_add);
 
             // Create a new menu item with the localized string from AppResources.
-            ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-            ApplicationBar.MenuItems.Add(appBarMenuItem);
+            ApplicationBarMenuItem AppBarMenu_DevSetup = new ApplicationBarMenuItem(AppResources.AppBarMenu_DevSetup);
+            AppBarMenu_DevSetup.Click += AppBarMenu_DevSetup_Click;
+            ApplicationBar.MenuItems.Add(AppBarMenu_DevSetup);
+        }
+
+        // This is setup code that loads sample images into the image library 
+        // This should be removed before publishing 
+        void AppBarMenu_DevSetup_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                string filename = "Sample" + i + ".jpg";
+                string filelocation = "Images/sample" + i + ".jpg";
+                library.SavePictureToCameraRoll(filename, Application.GetResourceStream(new Uri(@filelocation, UriKind.Relative)).Stream);
+            }
+            
+            MessageBox.Show("Dev Setup Complated. Sample images have been loaded");
         }
 
         private void AddButton_Click(object sender, EventArgs e)
